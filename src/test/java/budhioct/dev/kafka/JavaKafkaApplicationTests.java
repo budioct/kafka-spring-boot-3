5e8c10ff6111fa321f7f49126a91556314eee287
Budhi Octaviansyah <budioct@gmail.com>
2024-12-18 17:23:29 +0700

    start project

---
+++ src/test/java/budhioct/dev/kafka/JavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package budhioct.dev.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
